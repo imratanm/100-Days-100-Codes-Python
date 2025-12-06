c3551e65aeffa4504cfeb3b5bf66fdf1b0a6a073
RATAN MONDAL <ratanmondal130@gmail.com>
2025-12-07 00:08:18 +0530

    Delete Day1-Name Generator directory

--- Day1-Name Generator/main.py
+++
@@ -1,7 +0,0 @@
-print("Welcome to the Band Name Generator!")
-
-place = input("Please enter where you have born: ")
-pet = input("Please enter your pet name: ")
-
-print("Your Band name could be: " + place + " " + pet)
-print("Thank you for using the Band Name Generator!")
