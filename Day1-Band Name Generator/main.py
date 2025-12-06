dbed68500e6b9eb6dc9553f870069191a0eac33f
RATAN MONDAL <ratanmondal130@gmail.com>
2025-12-07 00:08:35 +0530

    Add files via upload

---
+++ Day1-Band Name Generator/main.py
@@ -0,0 +1,7 @@
+print("Welcome to the Band Name Generator!")
+
+place = input("Please enter where you have born: ")
+pet = input("Please enter your pet name: ")
+
+print("Your Band name could be: " + place + " " + pet)
+print("Thank you for using the Band Name Generator!")
